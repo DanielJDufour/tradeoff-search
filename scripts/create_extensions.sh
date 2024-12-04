8d6ef0b30c47afc7d2f2c5b7ba43a4e7038af12c
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-03 21:25:40 -0500

    Update create_extensions.sh

--- scripts/create_extensions.sh
+++ scripts/create_extensions.sh
@@ -1,5 +1,3 @@
 #!/bin/sh -e
 
-sudo systemctl start postgresql.service
-
-psql -h 0.0.0.0 -p 5432 --username postgres seasketch < ../sql/create_extensions.sql
+psql -h postgres -p 5432 --username postgres seasketch < ../sql/create_extensions.sql
