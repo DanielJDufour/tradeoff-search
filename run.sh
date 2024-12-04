904e1402b924c2dbb9eaca0c6547235cf6a8d304
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-03 21:30:43 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -e
 
-sh -c "cd ./scripts && ./create_extensions.sh"
+psql -h postgres -p 5432 --username postgres seasketch < ../sql/create_extensions.sql
+
+#sh -c "cd ./scripts && ./create_extensions.sh"
