eeabb37339ca02ca91505d8265035a008782da07
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-06 00:14:13 -0500

    Update setup_db.sh

--- setup_db.sh
+++ setup_db.sh
@@ -7,7 +7,7 @@ echo "creating seasketch database"
 sudo -Hiu postgres psql -c "CREATE DATABASE seasketch;"
 
 echo "creating extensions"
-sudo -Hiu postgres psql -d seasketch < "$PWD/sql/create_extensions.sql"
+sudo -Hiu postgres psql seasketch < "$PWD/sql/create_extensions.sql"
 
 echo "loading h3 extension"
 # sudo -Hiu postgres pgxn load -d seasketch h3
