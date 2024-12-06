bd374f483adc8faae73d1c156546f829ccbcdb56
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-05 20:22:10 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,10 +6,15 @@ sudo apt-get install -y build-essential curl git
       
 sudo apt-get install -y pgxnclient postgresql-14 postgresql-14-postgis-3 postgresql-server-dev-14        
 
-apt-cache search h3
-      
-apt-cache search postgresql
-
 sudo pgxn install h3
 
-# sudo pgxn load -d seasketch -h 127.0.0.1 -U postgres h3
+# start postgresql database service
+sudo systemctl start postgresql.service
+
+# test database connection
+psql -U postgres -c 'SELECT 1;'
+
+# create database
+# psql -U postgres -c 'CREATE DATABASE ;'
+
+# sudo pgxn load -d seasketch -U postgres h3
