8f6c79569abe5c99e11303e72dbd32919fe4787e
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-06 18:24:01 -0500

    Delete setup_table.sh

--- setup_table.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-
-echo "starting database"
-sudo systemctl start postgresql.service
-
-echo "creating sketches table"
-chmod 764 "$PWD/sql/create_sketches_table.sql"
-sudo -Hiu postgres psql -f "$PWD/sql/create_sketches_table.sql"
