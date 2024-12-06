53d843625a2af6db2095640f0c31553b456d6d1f
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-06 14:37:34 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ echo "starting database"
 sudo systemctl start postgresql.service
 
 echo "creating materialize views and indices"
-sudo -Hiu postgres psql -d seasketch < "$PWD/v1/setup.sql"
+sudo -Hiu postgres psql -U postgres < "$PWD/v1/setup.sql"
 
 echo "run performance tests"
-sudo -Hiu postgres psql -d seasketch < "$PWD/v1/test.sql"
+sudo -Hiu postgres psql -U postgres < "$PWD/v1/test.sql"
