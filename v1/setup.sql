077135e57e1ed56960583bf86d23c39a17daacf6
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-06 14:15:15 -0500

    Update setup.sql

--- v1/setup.sql
+++ v1/setup.sql
@@ -4,11 +4,13 @@ CREATE EXTENSION IF NOT EXISTS h3 CASCADE;
 CREATE EXTENSION IF NOT EXISTS h3_postgis CASCADE;
 
 --- debug
---- SELECT * FROM pg_namespace;
+- SELECT * FROM pg_namespace;
 
--- SELECT format('%I.%I(%s)', ns.nspname, p.proname, oidvectortypes(p.proargtypes)) 
--- FROM pg_proc p INNER JOIN pg_namespace ns ON (p.pronamespace = ns.oid)
--- WHERE ns.nspname = 'public';
+SELECT format('%I.%I(%s)', ns.nspname, p.proname, oidvectortypes(p.proargtypes)) 
+FROM pg_proc p INNER JOIN pg_namespace ns ON (p.pronamespace = ns.oid)
+WHERE ns.nspname = 'public';
+
+\dp pg_catalog.pg_authid;
 
 SHOW search_path;
 
