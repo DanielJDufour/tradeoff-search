0c05d8312ce3a7a3a88ec344e2686ed2084dd48c
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-06 18:03:21 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@
 
 sudo apt-get update
 
-sudo apt-get install -qq -y build-essential curl git
+sudo apt-get install -y build-essential curl git
 
 # remove default (older version of) postgresql
 sudo apt-get --purge remove postgresql-14
 
 echo | sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
       
-sudo apt-get install -qq -y pgxnclient postgresql-17 postgresql-client-17 postgresql-17-postgis-3 postgresql-server-dev-17       
+sudo apt-get install -y pgxnclient postgresql-17 postgresql-client-17 postgresql-17-postgis-3 postgresql-server-dev-17       
 
 sudo pgxn install h3
 
@@ -28,5 +28,8 @@ sudo -Hiu postgres psql -c 'CREATE EXTENSION postgis_raster';
 sudo -Hiu postgres psql -c 'CREATE EXTENSION h3';
 sudo -Hiu postgres psql -c 'CREATE EXTENSION h3_postgis';
 
---- check that postgis is installed properly
+# check that postgis is installed properly
 sudo -Hiu postgres psql -c 'SELECT ST_Point(-71.104, 42.315);'
+
+# create main sketches table
+sudo -Hiu postgres psql < ./sql/create_sketches_table.sql
