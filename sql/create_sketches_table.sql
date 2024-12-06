9311ea512b15f82285d6b00343a5ea93ef44d435
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-06 14:44:06 -0500

    Update create_sketches_table.sql

--- sql/create_sketches_table.sql
+++ sql/create_sketches_table.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION public.create_bbox(geom public.geometry, sketch_id integer) RETURNS real[]
+CREATE OR REPLACE FUNCTION create_bbox(geom geometry, sketch_id integer) RETURNS real[]
   LANGUAGE plpgsql IMMUTABLE SECURITY DEFINER
   AS $$
   declare
@@ -12,24 +12,24 @@ CREATE OR REPLACE FUNCTION public.create_bbox(geom public.geometry, sketch_id in
   end;
 $$;
 
-CREATE TABLE public.sketches (
+CREATE TABLE sketches (
     id SERIAL,
     -- name text NOT NULL,
     -- sketch_class_id integer NOT NULL,
     -- user_id integer,
     -- collection_id integer,
     -- copy_of integer,
-    user_geom public.geometry(Geometry,4326),
-    geom public.geometry(Geometry,4326),
+    user_geom geometry(Geometry,4326),
+    geom geometry(Geometry,4326),
     -- folder_id integer,
     -- properties jsonb DEFAULT '{}'::jsonb NOT NULL,
-    num_vertices integer GENERATED ALWAYS AS (public.st_npoints(COALESCE(geom, user_geom))) STORED,
+    num_vertices integer GENERATED ALWAYS AS (st_npoints(COALESCE(geom, user_geom))) STORED,
     -- form_element_id integer,
     -- response_id integer,
-    mercator_geometry public.geometry(Geometry,3857) GENERATED ALWAYS AS (public.st_transform(COALESCE(geom, user_geom), 3857)) STORED,
+    mercator_geometry geometry(Geometry,3857) GENERATED ALWAYS AS (st_transform(COALESCE(geom, user_geom), 3857)) STORED,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     updated_at timestamp with time zone DEFAULT now() NOT NULL,
     -- post_id integer,
     shared_in_forum boolean DEFAULT false NOT NULL,
-    bbox real[] GENERATED ALWAYS AS (public.create_bbox(COALESCE(geom, user_geom), id)) STORED
+    bbox real[] GENERATED ALWAYS AS (create_bbox(COALESCE(geom, user_geom), id)) STORED
 );
