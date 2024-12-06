764b9d0715a979ea7e9e3a6722f0eb147670073e
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-05 22:09:49 -0500

    Update setup_db.sh

--- setup_db.sh
+++ setup_db.sh
@@ -4,7 +4,7 @@ echo "starting database"
 sudo systemctl start postgresql.service
 
 echo "creating seasketch database"
-sudo -Hiu postgres psql seasketch -c "CREATE DATABASE seasketch;"
+sudo -Hiu postgres psql -c "CREATE DATABASE seasketch;"
 
 echo "creating postgis extension"
 sudo -Hiu postgres psql seasketch -c "CREATE EXTENSION postgis;"
