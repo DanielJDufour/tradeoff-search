eec3f4a245f5907c961bb24516d1b3a020f757e0
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-06 14:42:51 -0500

    Update setup_table.sh

--- setup_table.sh
+++ setup_table.sh
@@ -4,4 +4,4 @@ echo "starting database"
 sudo systemctl start postgresql.service
 
 echo "creating sketches table"
-sudo -Hiu postgres psql -U postgres < "$PWD/sql/create_sketches_table.sql"
+sudo -Hiu postgres psql -f "$PWD/sql/create_sketches_table.sql"
