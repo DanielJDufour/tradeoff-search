4a2cf8027879c2daeac1c00abedc2cd6c506284e
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-06 18:30:53 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ sudo apt-get --purge remove postgresql-14
 
 echo | sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
       
-sudo apt-get install -y pgxnclient postgresql-17 postgresql-client-17 postgresql-17-postgis-3 postgresql-server-dev-17       
+sudo apt-get install -y pgxnclient postgresql-16 postgresql-client-16 postgresql-16-postgis-3 postgresql-server-dev-16      
 
 sudo pgxn install h3
 
