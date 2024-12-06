41331077e68811ed5dae4a9ed9322e3876e4b4d7
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-06 17:48:06 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@
 
 sudo apt-get update
 
-sudo apt-get install -y build-essential curl git
+sudo apt-get install -qq -y build-essential curl git
 
 # remove default (older version of) postgresql
 sudo apt-get --purge remove postgresql-14
 
 echo | sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
       
-sudo apt-get install -y pgxnclient postgresql-17 postgresql-client-17 postgresql-17-postgis-3 postgresql-server-dev-17       
+sudo apt-get install -qq -y pgxnclient postgresql-17 postgresql-client-17 postgresql-17-postgis-3 postgresql-server-dev-17       
 
 sudo pgxn install h3
 
