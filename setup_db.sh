e5eca316c6d007bfef4f3fd13f4d70651b0af127
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-06 14:29:43 -0500

    Update setup_db.sh

--- setup_db.sh
+++ setup_db.sh
@@ -6,5 +6,7 @@ sudo systemctl start postgresql.service
 echo "creating seasketch database"
 sudo -Hiu postgres psql -U postgres -c "CREATE DATABASE seasketch;"
 
+sudo -Hiu postgres psql -U postgres -c "GRANT USAGE ON SCHEMA public TO postgres;"
+
 echo "creating extensions"
 sudo -Hiu postgres psql -U postgres seasketch < "$PWD/sql/create_extensions.sql"
