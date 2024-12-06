98fdf53c5be9bf6ca7087c6f47e27410dcda2f34
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-05 23:00:28 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y postgresql-common
 
 echo | sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
       
-sudo apt-get install -y pgxnclient postgresql-16 postgresql-client-16 postgresql-16-postgis-3 postgresql-server-dev-16       
+sudo apt-get install -y pgxnclient postgresql-17 postgresql-client-17 postgresql-17-postgis-3 postgresql-server-dev-17       
 
 sudo pgxn install h3
 
@@ -16,4 +16,4 @@ echo "starting database"
 sudo systemctl start postgresql.service
 
 echo "testing database connection"
-sudo -Hiu postgres psql -c 'SELECT 1;'
+sudo -Hiu postgres psql -c 'SELECT version();'
