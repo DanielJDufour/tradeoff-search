d30afca78a845e0577825846875d8749efabefb4
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-06 19:25:59 -0500

    Update setup.sql

--- v1/setup.sql
+++ v1/setup.sql
@@ -1,37 +1,21 @@
-SELECT ST_Dump(ST_Point(-71.104, 42.315));
-
--- --- debug
--- SELECT * FROM pg_namespace;
-
--- -- SELECT format('%I.%I(%s)', ns.nspname, p.proname, oidvectortypes(p.proargtypes)) 
--- -- FROM pg_proc p INNER JOIN pg_namespace ns ON (p.pronamespace = ns.oid)
--- -- WHERE ns.nspname = 'public';
-
--- SELECT current_user;
-
--- SELECT * FROM pg_authid;
-
--- SHOW search_path;
-
-CREATE MATERIALIZED VIEW cell_data AS 
+DROP MATERIALIZED VIEW IF EXISTS cells CASCADE;
+CREATE MATERIALIZED VIEW cells AS 
+WITH cell_data AS(
   SELECT
     sketches.id AS sketch_id,
     poly_area,
     cells0,
     cells3,
     cells5,
-    cells7,
-    cells9
+    cells7
   FROM sketches
-  LEFT JOIN LATERAL ST_Dump(geom::geometry) AS polys ON true
+  LEFT JOIN LATERAL ST_Dump(geom) AS polys ON true
   LEFT JOIN LATERAL ST_Area(polys.geom) AS poly_area ON true
-  LEFT JOIN LATERAL h3_polygon_to_cells(polys.geom, 9) AS cells9 ON true
-  LEFT JOIN LATERAL h3_cell_to_parent(cells9, 7) AS cells7 ON true
+  LEFT JOIN LATERAL h3_polygon_to_cells(polys.geom, 7) AS cells7 ON true
   LEFT JOIN LATERAL h3_cell_to_parent(cells7, 5) AS cells5 ON true
   LEFT JOIN LATERAL h3_cell_to_parent(cells5, 3) AS cells3 ON true
-  LEFT JOIN LATERAL h3_cell_to_parent(cells3, 0) AS cells0 ON true;
-
-CREATE MATERIALIZED VIEW cells AS 
+  LEFT JOIN LATERAL h3_cell_to_parent(cells3, 0) AS cells0 ON true
+)
 (SELECT sketch_id, 0 AS res, cells0 AS cell_id, MAX(1 / poly_area) AS cell_weight FROM cell_data GROUP BY sketch_id, cells0 ORDER BY sketch_id)
 UNION
 (SELECT sketch_id, 3 AS res, cells3 AS cell_id, MAX(1 / poly_area) AS cell_weight FROM cell_data GROUP BY sketch_id, cells3 ORDER BY sketch_id)
@@ -39,15 +23,11 @@ UNION
 (SELECT sketch_id, 5 AS res, cells5 AS cell_id, MAX(1 / poly_area) AS cell_weight FROM cell_data GROUP BY sketch_id, cells5 ORDER BY sketch_id)
 UNION
 (SELECT sketch_id, 7 AS res, cells7 AS cell_id, MAX(1 / poly_area) AS cell_weight FROM cell_data GROUP BY sketch_id, cells7 ORDER BY sketch_id);
-UNION
-(SELECT sketch_id, 9 AS res, cells9 AS cell_id, MAX(1 / poly_area) AS cell_weight FROM cell_data GROUP BY sketch_id, cells9 ORDER BY sketch_id);
 
-SELECT * FROM cells LIMIT 5;
-
+DROP VIEW IF EXISTS sketch_weights;
 CREATE VIEW sketch_weights AS SELECT sketch_id, res, SUM(cell_weight) AS wt FROM cells GROUP BY sketch_id, res;
 
-SELECT * FROM sketch_weights LIMIT 5;
-
+DROP MATERIALIZED VIEW all_cells;
 CREATE MATERIALIZED VIEW all_cells AS 
 SELECT
   c.sketch_id,
@@ -62,8 +42,7 @@ CREATE INDEX idx_all_cells_sketch_res ON all_cells (res);
 CREATE INDEX idx_all_cells_cell_id ON all_cells (cell_id);
 CREATE INDEX idx_all_cells_cell_weight ON all_cells (cell_weight);
 
-SELECT * FROM all_cells LIMIT 5;
-
+DROP MATERIALIZED VIEW IF EXISTS sketch_geometry_hashes CASCADE;
 CREATE MATERIALIZED VIEW sketch_geometry_hashes AS 
   SELECT
     id AS sketch_id,
@@ -79,5 +58,3 @@ CREATE INDEX idx_sketch_geometry_hashes_sketch_id ON sketch_geometry_hashes (ske
 CREATE INDEX idx_sketch_geometry_hashes_num_geometries ON sketch_geometry_hashes (num_geometries);
 CREATE INDEX idx_sketch_geometry_hashes_poly_hash ON sketch_geometry_hashes (poly_hash);
 CREATE INDEX idx_sketch_geometry_hashes_wt ON sketch_geometry_hashes (wt);
-
-SELECT * FROM sketch_geometry_hashes LIMIT 5;
