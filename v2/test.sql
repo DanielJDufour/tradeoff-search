e3d64c53398d46dab9a2a457117f41466b8f8d63
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-08 17:43:40 -0500

    Update test.sql

--- v2/test.sql
+++ v2/test.sql
@@ -1,5 +1,10 @@
 \timing
 
+REFRESH MATERIALIZED VIEW CONCURRENTLY cells;
+REFRESH MATERIALIZED VIEW CONCURRENTLY all_cells;
+REFRESH MATERIALIZED VIEW CONCURRENTLY sketch_geometry_hashes;
+
+
 SELECT * FROM get_similar_sketches(1);
 SELECT * FROM get_similar_sketches(1234);
 SELECT * FROM get_similar_sketches(5678);
