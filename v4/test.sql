9efbd817370b4673c5d8f73c602266cbd5032a55
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-12 16:42:50 -0500

    Create test.sql

---
+++ v4/test.sql
@@ -0,0 +1,7 @@
+\timing
+
+SELECT * FROM get_similar_sketches(1);
+SELECT * FROM get_similar_sketches(1234);
+SELECT * FROM get_similar_sketches(5678);
+SELECT * FROM get_similar_sketches(10000);
+SELECT * FROM get_similar_sketches(10001);
