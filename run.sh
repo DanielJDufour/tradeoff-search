6a808534fd7328d737567a745af99a2f6f4219a5
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-12 16:21:18 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -47,6 +47,9 @@ sh -c "cd sql && unzip -qq seed.sql.zip"
 
 sudo -Hiu postgres psql -q < ./sql/seed.sql
 
+# seed collection, a sketch with null geometry
+sudo -Hiu postgres psql -c "INSERT INTO sketches (geom) VALUES (NULL);"
+
 # sudo -Hiu postgres psql < ./v1/setup.sql
 # sudo -Hiu postgres psql < ./v1/test.sql
 # sudo -Hiu postgres psql < ./v1/test2.sql
