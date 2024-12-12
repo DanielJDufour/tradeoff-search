25cc135d54bc4ee1a3d58009b81c7dc44a6044ae
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-12 17:42:56 -0500

    Update test.sql

--- v4/test.sql
+++ v4/test.sql
@@ -5,3 +5,4 @@ SELECT * FROM get_similar_sketches(1234);
 SELECT * FROM get_similar_sketches(5678);
 SELECT * FROM get_similar_sketches(10000);
 SELECT * FROM get_similar_sketches(10001);
+SELECT * FROM get_similar_sketches(10002);
