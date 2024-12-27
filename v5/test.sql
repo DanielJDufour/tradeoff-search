c0cb5edce073b68050978a48f59d137d44476f3a
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-12-27 00:06:52 -0500

    Update test.sql

--- v5/test.sql
+++ v5/test.sql
@@ -5,4 +5,6 @@ SELECT * FROM get_similar_sketches(1234);
 SELECT * FROM get_similar_sketches(5678);
 SELECT * FROM get_similar_sketches(10000);
 SELECT * FROM get_similar_sketches(10001);
+
+SELECT id, ST_AsGeoJSON(geom) FROM sketches WHERE id = 10002;
 SELECT * FROM get_similar_sketches(10002);
