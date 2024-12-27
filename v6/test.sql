a059f6eb474a59d02bd85082c1982b64360dbe3a
DanielJDufour <daniel.j.dufour@gmail.com>
2024-12-27 02:00:52 -0500

    faster

--- v6/test.sql
+++ v6/test.sql
@@ -8,3 +8,14 @@ SELECT * FROM get_similar_sketches(10001);
 
 SELECT id, ST_AsGeoJSON(geom) FROM sketches WHERE id = 10002;
 SELECT * FROM get_similar_sketches(10002);
+
+\echo 'now faster'
+SELECT * FROM get_similar_sketches_faster(1);
+SELECT * FROM get_similar_sketches_faster(1234);
+SELECT * FROM get_similar_sketches_faster(5678);
+SELECT * FROM get_similar_sketches_faster(10000);
+SELECT * FROM get_similar_sketches_faster(10001);
+
+SELECT id, ST_AsGeoJSON(geom) FROM sketches WHERE id = 10002;
+SELECT * FROM get_similar_sketches_faster(10002);
+
