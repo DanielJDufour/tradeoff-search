3c1e4869948dfe55845cbc057d0c36ea67351735
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2025-01-01 19:20:35 -0500

    Update test.sql

--- v8/test.sql
+++ v8/test.sql
@@ -3,7 +3,11 @@
 \echo 'starting test.sql'
 
 SELECT * FROM get_similar_sketches(1);
+SELECT * FROM get_similar_sketches(100);
 SELECT * FROM get_similar_sketches(1234);
+SELECT * FROM get_similar_sketches(5000);
+SELECT * FROM get_similar_sketches(5001);
+SELECT * FROM get_similar_sketches(5002);
 SELECT * FROM get_similar_sketches(5678);
 SELECT * FROM get_similar_sketches(10000);
 SELECT * FROM get_similar_sketches(10001);
